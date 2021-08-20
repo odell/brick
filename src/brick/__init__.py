9e9cca97064b903b3a9bb44e678a07dc4ad70051
Daniel Odell <dmcodelljr@gmail.com>
2021-08-20 15:28:12 -0400

    Adds a __version__ number.

---
+++ src/brick/__init__.py
@@ -0,0 +1,8 @@
+with open('setup.cfg', 'r') as f:
+    cfg = f.read()
+
+version_string = list(filter(
+    lambda s: 'version' in s, cfg.split('\n')
+))[0].split()[2]
+
+__version__ = version_string
